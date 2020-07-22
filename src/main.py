8dc1fb53a2ed240bfc2a44827dd34bf89b249a1d
David <david.richter@freenet.de>
2020-07-22 22:11:52 +0200

    line break

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ class Main:
             track = dlibTracker()
         else:
             track = ocvTracker()
-            
+
         track.initTracker()
 
         file = load.loadList(inputfile)
